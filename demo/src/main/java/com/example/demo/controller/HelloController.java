abaf9e68c4358756cbe4fb91e1e0c531728e2f21
qyj <qyj@192.168.1.2>
2018-04-02 21:34:22 +0800

    qyj

---
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+//	@Autowired
+//	private HttpClient client;
+	
+	@GetMapping(value = "/hello")
+	public String hello(){
+//		String url = "http://localhost:8081/user/";
+//		HttpMethod method = HttpMethod.GET;
+//		MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
+//		params.add("name", "qinyongjia");
+//		return client.client(url, method, params);
+		return "hello !";
+		
+	}
+	
+
+}
